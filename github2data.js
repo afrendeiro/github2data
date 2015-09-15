84d8bcd97769f0600c76e3fd8e8f48a941e4a7cb
Andre Rendeiro <afrendeiro@gmail.com>
2015-09-15 08:32:04 +0200

    add pipeline calling function, project-specific options

--- github2data.js
+++ github2data.js
@@ -1,28 +1,58 @@
+// 4 things to edit:
+// add here your github repo name (should match the project name to be run)
+var projectName = 'name'
+// this should point to the annotation sheet with a relative path within the repository
+var metadata = 'metadata/sequencing_annotation.csv'
+// add here the URL to the github webhook:
+var webhook = ''
+// add here your github secret for this webhook:
+var secret = ''
+
+
+// don't edit below!
+function runPipelines(projectName, metadata) {
+    var exec = require('child_process').exec, child;
+    var cmd = 'pipelines ' + projectName + ' ' + metadata;
+
+    child = exec(
+            cmd,
+            function (error, stdout, stderr) {
+                    console.log('stdout: ' + stdout);
+                    console.log('stderr: ' + stderr);
+                    if (error !== null) {
+                             console.log('exec error: ' + error);
+                    }
+            });
+     child();
+}
+
 var http = require('http')
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhook', secret: 'myhashsecret' })
+var handler = createHandler({ path: webhook, secret: secret})
 
 http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
-  })
+    handler(req, res, function (err) {
+        res.statusCode = 404
+        res.end('no such location')
+    })
 }).listen(7777)
 
 handler.on('error', function (err) {
-  console.error('Error:', err.message)
+    console.error('Error:', err.message)
 })
 
 handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref)
+    console.log('Received a push event for %s to %s',
+        event.payload.repository.name,
+        event.payload.ref)
+    // run the pipelines!
+    runPipelines(projectName, metadata)
 })
 
 handler.on('issues', function (event) {
-  console.log('Received an issue event for %s action=%s: #%d %s',
-    event.payload.repository.name,
-    event.payload.action,
-    event.payload.issue.number,
-    event.payload.issue.title)
+    console.log('Received an issue event for %s action=%s: #%d %s',
+        event.payload.repository.name,
+        event.payload.action,
+        event.payload.issue.number,
+        event.payload.issue.title)
 })
